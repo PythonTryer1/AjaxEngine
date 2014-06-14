8544c344529b43931da499c12f27f33056f79896
houfeng <admin@xhou.net>
2014-06-14 11:48:07 +0800

    dev

--- AjaxEngine.Demo/Handler1.ashx.cs
+++ AjaxEngine.Demo/Handler1.ashx.cs
@@ -38,13 +38,24 @@ namespace AjaxEngine.Demo
         [AjaxMethod(HttpMethod = "POST")]
         public T add(T t)
         {
+            Context.Response.Cookies.Add(new HttpCookie("who", "houfeng"));
             t.t1++;
             t.t2++;
             return t;
         }
+
+        [AjaxMethod]
+        public string create()
+        {
+            HttpCookie cookie = new HttpCookie("who", "houfeng");
+            cookie.Expires = DateTime.Now.AddYears(100);
+            Context.Response.Cookies.Add(cookie);
+            return "ok";
+        }
         [AjaxMethod]
         public Bitmap getImage(int id)
         {
+
             return new Bitmap(100, 100);
         }
     }
